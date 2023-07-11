eb325fc6cda4913ff2c9742a4fd969597c29f586
HoustonCharly <jriverar1801@alumno.ipn.mx>
2023-07-11 15:33:51 -0600

    First Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Practica_6_Feedforward
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.checkXOR = new System.Windows.Forms.CheckBox();
+            this.checkEjercicio = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.TablaGridView1 = new System.Windows.Forms.DataGridView();
+            this.lBsalida = new System.Windows.Forms.ListBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.firmaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.TablaGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // checkXOR
+            // 
+            this.checkXOR.Font = new System.Drawing.Font("OCR A Extended", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkXOR.Location = new System.Drawing.Point(35, 37);
+            this.checkXOR.Name = "checkXOR";
+            this.checkXOR.Size = new System.Drawing.Size(88, 25);
+            this.checkXOR.TabIndex = 0;
+            this.checkXOR.Text = "XOR";
+            this.checkXOR.UseVisualStyleBackColor = true;
+            this.checkXOR.CheckedChanged += new System.EventHandler(this.checkXOR_CheckedChanged);
+            // 
+            // checkEjercicio
+            // 
+            this.checkEjercicio.Font = new System.Drawing.Font("OCR A Extended", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkEjercicio.Location = new System.Drawing.Point(33, 65);
+            this.checkEjercicio.Name = "checkEjercicio";
+            this.checkEjercicio.Size = new System.Drawing.Size(118, 27);
+            this.checkEjercicio.TabIndex = 1;
+            this.checkEjercicio.Text = "EJERCICIO";
+            this.checkEjercicio.UseVisualStyleBackColor = true;
+            this.checkEjercicio.CheckedChanged += new System.EventHandler(this.checkEjercicio_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("OCR A Extended", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(182, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 35);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "FeedForward";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // TablaGridView1
+            // 
+            this.TablaGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.TablaGridView1.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.TablaGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.TablaGridView1.Location = new System.Drawing.Point(8, 101);
+            this.TablaGridView1.Name = "TablaGridView1";
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Bahnschrift Light", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TablaGridView1.RowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.TablaGridView1.Size = new System.Drawing.Size(416, 207);
+            this.TablaGridView1.TabIndex = 3;
+            // 
+            // lBsalida
+            // 
+            this.lBsalida.Font = new System.Drawing.Font("Bahnschrift", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lBsalida.FormattingEnabled = true;
+            this.lBsalida.ItemHeight = 16;
+            this.lBsalida.Items.AddRange(new object[] {
+            " "});
+            this.lBsalida.Location = new System.Drawing.Point(431, 12);
+            this.lBsalida.Name = "lBsalida";
+            this.lBsalida.Size = new System.Drawing.Size(233, 292);
+            this.lBsalida.TabIndex = 4;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.Info;
+            this.menuStrip1.Font = new System.Drawing.Font("OCR A Extended", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.firmaToolStripMenuItem,
+            this.salirToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(671, 25);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // firmaToolStripMenuItem
+            // 
+            this.firmaToolStripMenuItem.Name = "firmaToolStripMenuItem";
+            this.firmaToolStripMenuItem.Size = new System.Drawing.Size(65, 21);
+            this.firmaToolStripMenuItem.Text = "Firma";
+            this.firmaToolStripMenuItem.Click += new System.EventHandler(this.firmaToolStripMenuItem_Click);
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(65, 21);
+            this.salirToolStripMenuItem.Text = "Salir";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(671, 315);
+            this.Controls.Add(this.lBsalida);
+            this.Controls.Add(this.TablaGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkEjercicio);
+            this.Controls.Add(this.checkXOR);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "FEEDFORWARD";
+            ((System.ComponentModel.ISupportInitialize)(this.TablaGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkXOR;
+        private System.Windows.Forms.CheckBox checkEjercicio;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView TablaGridView1;
+        private System.Windows.Forms.ListBox lBsalida;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem firmaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Practica_6_Feedforward
+{
+    public partial class Form1 : Form
+    {
+        double peso111 = 5.191129;  double peso112 = 2.758669;
+        double peso121 = 5.473012;  double peso122 = 2.769596;
+        double peso21 = 5.839709;   double peso22 = -6.186834;
+
+        double umbral10 = -1.90289; double umbral11 = -4.127002;
+        double umbral20 = -2.570539;
+
+        float W2 = 1;
+        double U2 = 0.5;
+
+        private void checkXOR_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkXOR.Checked)
+            {
+                TablaGridView1.ClearSelection();
+                TablaGridView1.Columns.Clear();
+                lBsalida.Items.Clear();
+                TablaGridView1.Columns.Add("x1", "X1");
+                TablaGridView1.Columns.Add("x2", "X2");
+                TablaGridView1.Columns.Add("ye", "Yesperada");
+                TablaGridView1.Columns.Add("y0", "Yobt");
+                TablaGridView1.Columns[3].DefaultCellStyle.BackColor =Color.Cyan;
+                TablaGridView1.Columns[2].DefaultCellStyle.BackColor =Color.Lavender;
+
+
+                TablaGridView1.Rows.Add("0", "0", "1");
+                TablaGridView1.Rows.Add("0", "1", "0");
+                TablaGridView1.Rows.Add("1", "0", "0");
+                TablaGridView1.Rows.Add("1", "1", "1");
+            }
+        }
+        private void checkEjercicio_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkEjercicio.Checked)
+            {
+                TablaGridView1.ClearSelection();
+                TablaGridView1.Columns.Clear();
+                lBsalida.Items.Clear();
+                TablaGridView1.Columns.Add("x1", "X1");
+                TablaGridView1.Columns.Add("x2", "X2");
+                TablaGridView1.Columns.Add("y1", "Y1");
+                TablaGridView1.Columns.Add("y1", "Y2");
+                TablaGridView1.Columns[3].DefaultCellStyle.BackColor =Color.Lavender;
+                TablaGridView1.Columns[2].DefaultCellStyle.BackColor =Color.Lavender;
+                TablaGridView1.Rows.Add("0", "0");
+                TablaGridView1.Rows.Add("0", "1");
+                TablaGridView1.Rows.Add("1", "0");
+                TablaGridView1.Rows.Add("1", "1");
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lBsalida.Items.Clear();
+            if (checkXOR.Checked)
+            {
+
+                lBsalida.Items.Add("\nPatron 1");
+                lBsalida.Items.Add("\n");
+                
+                TablaGridView1.Rows[0].Cells[3].Value = entradaXOR(0, 0);
+
+                lBsalida.Items.Add("\nPatron 2");
+                lBsalida.Items.Add("\n");
+                TablaGridView1.Rows[1].Cells[3].Value = entradaXOR(0, 1);
+
+                lBsalida.Items.Add("\nPatron 3");
+                lBsalida.Items.Add("\n"); 
+                TablaGridView1.Rows[2].Cells[3].Value = entradaXOR(1, 0);
+
+                lBsalida.Items.Add("\nPatron 4");
+                lBsalida.Items.Add("\n");
+                TablaGridView1.Rows[3].Cells[3].Value = entradaXOR(1, 1);
+            }
+            else if (checkEjercicio.Checked)
+            {
+                lBsalida.Items.Add("Patron 1");
+                lBsalida.Items.Add("\n");
+                TablaGridView1.Rows[0].Cells[2].Value =ejercicio11(0, 0);
+                TablaGridView1.Rows[0].Cells[3].Value =ejercicio22(0, 0);
+                lBsalida.Items.Add("Patron 2");
+                lBsalida.Items.Add("\n");
+                TablaGridView1.Rows[1].Cells[2].Value =ejercicio11(0, 1);
+                TablaGridView1.Rows[1].Cells[3].Value =ejercicio22(0, 1);
+                lBsalida.Items.Add("Patron 3");
+                lBsalida.Items.Add("\n");
+                TablaGridView1.Rows[2].Cells[2].Value =ejercicio11(1, 0);
+                TablaGridView1.Rows[2].Cells[3].Value =ejercicio22(1, 0);
+                lBsalida.Items.Add("Patron 4");
+                lBsalida.Items.Add("\n");
+                TablaGridView1.Rows[3].Cells[2].Value =ejercicio11(1, 1);
+                TablaGridView1.Rows[3].Cells[3].Value =ejercicio22(1, 1);
+
+            }
+        }
+        double entradaXOR(double entrada1, double entrada2)
+        {   
+            double salidaneurona11 = 1/(1+(Math.Exp(-(((entrada1*peso111)+(entrada2*peso121))+umbral10))));
+            lBsalida.Items.Add("Capa: 1 Activ:1  "+salidaneurona11);
+
+            double salidaneurona12 = 1/(1+(Math.Exp(-(((entrada1*peso112)+(entrada2*peso122))+umbral11))));
+            lBsalida.Items.Add("Capa: 1 Activ:2  "+salidaneurona12);
+
+            double salidaneuronay11 = 1/(1+(Math.Exp(-(((peso21*salidaneurona11)+(peso22*salidaneurona12))+umbral20))));
+            
+            lBsalida.Items.Add("\nCapa: 2 Activ:1  "+salidaneuronay11);
+            lBsalida.Items.Add("\n"); 
+
+            lBsalida.Items.Add("\nSalida : "+salidaneuronay11);
+            lBsalida.Items.Add("\n");
+
+            return salidaneuronay11;
+        }
+
+        double A11, A12, A21, A22, A31, A32;
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void firmaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           MessageBox.Show("\tRivera Rivera Juan Carlos");
+        }
+
+        double ejercicio11(double entrada1, double entrada2)
+        {   
+            //Capa 1
+            A11=A12=1/(1+(Math.Exp(-(((entrada1*W2)+(entrada2*W2)+U2)))));
+            lBsalida.Items.Add("Capa: 1 Activ:0  "+A11);
+            lBsalida.Items.Add("Capa: 1 Activ:1  "+A12);
+
+            lBsalida.Items.Add("\n");
+            //Capa2
+            A21=A22=1/(1+(Math.Exp(-(((A11*W2)+(A12*W2)+U2)))));
+            lBsalida.Items.Add("Capa: 2 Activ:0  "+A21);
+            lBsalida.Items.Add("Capa: 2 Activ:1  "+A22);
+
+            lBsalida.Items.Add("\n");
+            //Capa 3
+            A31=A32=1/(1+(Math.Exp(-(((A21*W2)+(A22*W2)+U2)))));
+            lBsalida.Items.Add("Capa: 3 Activ:0  "+A31);
+            lBsalida.Items.Add("Capa: 3 Activ:1  "+A32);
+
+            lBsalida.Items.Add("\n");
+
+            lBsalida.Items.Add("\nSalida Y1: "+A31);
+            lBsalida.Items.Add("\nSalida Y1: "+A32);
+            lBsalida.Items.Add("\n");
+
+            return A31;
+         
+        }
+        double ejercicio22(double entrada1, double entrada2)
+        {
+            //Capa 1
+            A12=A11=1/(1+(Math.Exp(-(((entrada1*W2)+(entrada2*W2)+U2)))));
+            //Capa2
+            A22=A21=1/(1+(Math.Exp(-(((A11*W2)+(A12*W2)+U2)))));
+            //Capa 3
+            A32=A31=1/(1+(Math.Exp(-(((A21*W2)+(A22*W2)+U2)))));
+            return A32;
+
+        }
+    }
+}
